ac708ec0575b077f9711c068b63915dd0ad205a0
Kingbond470 <mausamsingh470@gmail.com>
2021-07-22 21:26:11 +0530

    Splash Screen

--- FirstCry/app/src/main/java/com/example/firstcry/MainActivity.java
+++ FirstCry/app/src/main/java/com/example/firstcry/MainActivity.java
@@ -1,42 +1,46 @@
 package com.example.firstcry;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.MediaController;
+import android.view.MotionEvent;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
 
-    private VideoView videoViewIntro;
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initViews();
+        try {
+            VideoView videoHolder = new VideoView(this);
+            setContentView(videoHolder);
+            Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.introduction);
+            videoHolder.setVideoURI(video);
 
-
-        videoViewIntro.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.introduction);
-        MediaController mediaController = new MediaController(this);
-        //link mediaController to videoView
-        mediaController.setAnchorView(videoViewIntro);
-        //allow mediaController to control our videoView
-        videoViewIntro.setMediaController(mediaController);
-        videoViewIntro.start();
-        videoViewIntro.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                finish();
-                Intent intent=new Intent(MainActivity.this,AskUserActivity.class);
-                startActivity(intent);
-            }
-        });
+            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    jump();
+                }
+            });
+            videoHolder.start();
+        } catch (Exception ex) {
+            jump();
+        }
     }
 
-    private void initViews() {
-        videoViewIntro=findViewById(R.id.videoViewIntro);
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        jump();
+//        return true;
+//    }
+
+    private void jump() {
+        if (isFinishing())
+            return;
+        startActivity(new Intent(this, AskUserActivity.class));
+        finish();
     }
+
 }
