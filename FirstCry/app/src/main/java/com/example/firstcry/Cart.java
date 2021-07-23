50fa53d3e29b6d5971fff3a5040ca293a071d4a3
Kingbond470 <mausamsingh470@gmail.com>
2021-07-23 13:50:12 +0530

    Cart Layout - Navigation Bar

---
+++ FirstCry/app/src/main/java/com/example/firstcry/Cart.java
@@ -0,0 +1,15 @@
+package com.example.firstcry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cart extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
--- FirstCry/app/src/main/java/com/example/firstcry/Home_Page_Activity.java
+++ FirstCry/app/src/main/java/com/example/firstcry/Home_Page_Activity.java
@@ -2,7 +2,10 @@ package com.example.firstcry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.slider.Slider;
 
@@ -10,10 +13,25 @@ import java.util.List;
 
 public class Home_Page_Activity extends AppCompatActivity {
 
+    private ImageView ivCart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        initViews();
 
+        ivCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Home_Page_Activity.this,CartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void initViews() {
+        ivCart=findViewById(R.id.ivCart);
     }
 }
