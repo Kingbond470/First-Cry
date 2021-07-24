10e50228b32f12411705a98f5cf73da6281a9d3e
Kingbond470 <mausamsingh470@gmail.com>
2021-07-24 17:37:55 +0530

    Notifications - Layout

---
+++ FirstCry/app/src/main/java/com/example/firstcry/Notification_Fragments/Offers_Notifications.java
@@ -0,0 +1,23 @@
+package com.example.firstcry.Notification_Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.firstcry.R;
+
+
+public class Offers_Notifications extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart__items_, container, false);
+    }
+
+}
